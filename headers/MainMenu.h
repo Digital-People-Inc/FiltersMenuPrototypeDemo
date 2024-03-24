55df94f58ff47ee2d4007c8a69a8271c6a8bf0d2
raccoonhands1 <reptile12456@gmail.com>
2024-03-23 20:47:13 -0700

    merged

---
+++ headers/MainMenu.h
@@ -0,0 +1,9 @@
+#ifndef MAINMENU_H_
+#define MAINMENU_H_
+
+class Window{
+public:
+	int execute();		//call the execute script for the main menu
+};
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include "MainMenu.h"
+#include <opencv2/highgui.hpp>
+
+int main(int argc, char *argv[]) {
+
+
+	Window *window = new Window();
+
+	window->execute();
+
+	return 0;
+}
